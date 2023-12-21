ecbf7ec44b9bd4fc5d00625dc0ddc181d65826d2
Arthur Barata <arthurbs@motorola.com>
2023-12-21 01:29:33 -0400

    hotfix na release

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
       <Generic></Generic>
       esse commit foi feito na minha feature-1 com base na development esse
       commit foi feito na feature-2 ainda com base na development segundo commit
-      na feature-2 pra fazer squash
+      na feature-2 pra fazer squash hot fixx na release
     </div>
   );
 }
